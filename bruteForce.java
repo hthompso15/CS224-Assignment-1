1c397887d70a63cfedc3da2430b5c2d731c9aa9c
Harrison Thompson <hthompso@uvm.edu>
2022-02-05 10:03:19 -0500

    Complete

--- bruteForce.java
+++ bruteForce.java
@@ -1,3 +1,8 @@
+// Harrison Thompson
+// CS 224
+// Assignment 1
+// Brute force solution of the knapsack problem
+
 import java.util.Arrays;
 
 public class bruteForce {
@@ -9,8 +14,6 @@ public class bruteForce {
         int limit = 15;
 
         optimize(weights,values,limit);
-        System.out.print("Finished");
-
     }
 
     public static void optimize(int[] weights, int values[], int limit) {
@@ -22,18 +25,18 @@ public class bruteForce {
         int weightMax = 0;
 
         // For i to 2^(size of the set -1) 
-        for(int i = 0 ; i < Math.pow(2,N-1);i++) { 
+        for(int i = 0 ; i < Math.pow(2,N);i++) { 
             int weight = 0;
             int value = 0;
+            
             // Goes through and sets choose[j] to false
-            for(int j = 0; j < (N-1); j++) {
+            for(int j = 0; j < (N); j++) {
                     choose[j] = false;                       
             }
-
             // if the index is greater than zero and val is greater than zero
-            for(int j = 0; j < (N-1); j++) {
-                double val = Math.pow(2,(i-1));
-                if(j > 0 && val > 0) {
+            for(int j = 0; j < (N); j++) {
+                int val = (int)Math.pow(2,(j));
+                if((i & val) > 0) {
                     choose[j] = true;
                 }
             }
@@ -41,28 +44,35 @@ public class bruteForce {
             // build ith subsets
             
             for(int j = 0; j < (N); j++) {
+                
                 if (choose[j]) {    
                     weight += weights[j];
-                    value += values[j];
-                    System.out.println(weight+ "," + value);
-
                 }
-                
-                
-                if (weight <= limit) {
+            }   
+            if (weight <= limit) {
+                for(int j = 0; j < (N); j++) {
+                    if (choose[j]) {    
+                        value += values[j];
+                    }
+                }       
 
-                    if(value >= valueMax) {
-                        valueMax = value;
-                        weightMax = weight;
-                        best = Arrays.copyOf(choose, choose.length);
-                    }       
-                } 
-            }            
+                if(value > valueMax) {
+                    valueMax = value;
+                    weightMax = weight;
+                    best = Arrays.copyOf(choose, choose.length);
+                }       
+            }                 
         }
-        System.out.println(valueMax);
-        System.out.println(weightMax);
-        System.out.println(best);
 
-        System.out.println("Finished");      
+        System.out.println("Highest Posible Value: " + valueMax);
+        System.out.println("Corresponding Weight to highest value: " + weightMax);
+
+        System.out.print("Gems Included: ");
+
+        for(int i = 0; i < best.length; i++) {
+            if (best[i]) {
+                System.out.print((i + 1)+ ", ");
+            }
+        }
     }
 }
